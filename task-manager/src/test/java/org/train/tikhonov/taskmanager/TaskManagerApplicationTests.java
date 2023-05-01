44b254ee817335e6d57b95ef487857cb133640cd
Aleksandr-Tikhonov-02 <aleksandr.tihonov.02@mail.ru>
2023-05-01 18:03:33 +0300

    add mongo to docker compose

---
+++ task-manager/src/test/java/org/train/tikhonov/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.train.tikhonov.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
